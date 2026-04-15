1f010fd53f53da18e747dcbb57b6d16019ce0981
AndreaRey05 <andrea_reyes.05@hotmail.com>
2026-04-15 00:20:32 -0600

    actualizacion servidor2

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,5 +4,6 @@ const { login, registro } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/registro', registro);
+router.post('/registro-admin', registro)
 
 module.exports = router;
